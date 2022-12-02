459e179e529dbee09ba179e4ec1e375b3ac809d1
KatyBeard <borodashkina.ekaterina@yandex.ru>
2022-12-02 13:55:50 +0500

    Added folder 'Example010'

---
+++ Lecture2/Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { -158, 852, 332, 77, 482, 56, 66, 77, 88 };
+
+int n = array.Length;
+int find = 77;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
